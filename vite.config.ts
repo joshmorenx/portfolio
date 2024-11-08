b5f7ce7800d1dc90c0075e313731f7a62ac4f439
Joshua Moreno <37154328+joshmorenx@users.noreply.github.com>
2024-11-08 16:00:20 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',
+  build: {
+    outDir: 'dist',
+  },
   test: {
     globals: true,
     environment: 'jsdom',
