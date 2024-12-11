9085feb76c859e241da90446a18175e8090b0742
joshmorenx <joshuamoreno21@gmail.com>
2024-12-11 13:46:37 -0600

    feat: Added better styling with tailwindCSS

--- src/components/DevInfo.tsx
+++ src/components/DevInfo.tsx
@@ -29,10 +29,10 @@ export default function DevInfo({ isDesktop, isTablet }: DevInfoProps): JSX.Elem
                                 .typeString("<span style='background-color: #d17a00; border-radius: 5px; padding: 5px; color: black;'>Software Developer</span>")
                                 .pauseFor(1000)
                                 .deleteAll()
-                                .typeString("<span style='background-color: #2f45d4; border-radius: 5px; padding: 5px; color: black;'>UI/UX Designer</span>")
+                                .typeString("<span style='background-color: #afa5d4; border-radius: 5px; padding: 5px; color: black;'>UI/UX Designer</span>")
                                 .pauseFor(1000)
                                 .deleteAll()
-                                .typeString("<span style='background-color: #07f5a2; border-radius: 5px; padding: 5px; color: black;'>Computer Science Engineer</span>")
+                                .typeString("<span style='background-color: #07ffaa; border-radius: 5px; padding: 5px; color: black;'>Computer Science Engineer</span>")
                                 .pauseFor(1000)
                                 .deleteAll()
                                 .start()
@@ -41,7 +41,7 @@ export default function DevInfo({ isDesktop, isTablet }: DevInfoProps): JSX.Elem
                 </h6>
             </Box>
             <Box className={isDesktop ? "sliced" : isTablet ? "sliced-tablet" : "sliced-mobile"}>
-                <Box>
+                <Box sx={{ display: 'flex', justifyContent: 'center' }}>
                     <img className={(isDesktop ? "dev-profile" : isTablet ? "dev-profile-tablet" : "dev-profile-mobile") + " fadeInRight"} src={devImg} alt="" />
                 </Box>
             </Box>
