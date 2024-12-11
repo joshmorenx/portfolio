75bd4404ffed5eae7eb7e5d5536189a8b50c4cd8
joshmorenx <joshuamoreno21@gmail.com>
2024-12-11 15:34:49 -0600

    feat: added tailwind + flowbite in order to use components

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,19 @@
 import { Box } from "@mui/material";
+import { Carousel } from "flowbite-react";
 
-export default function Project () {
+export default function Project() {
     return (
         <Box className={"projects"} id="projects">
             <h1>Projects</h1>
+            <Box sx={{ display: 'block', alignItems: 'center', width: '50%', height: '50%' }}>
+                <Carousel>
+                    <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
+                    <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
+                    <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+                    <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+                    <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+                </Carousel>
+            </Box>
         </Box>
     )
 }
